2bf85cdb974117b66f339c4de5490221a9b16515
NataliyaMylyan <mnatal2015@gmail.com>
2021-12-17 23:02:28 +0200

    axios-corrections

--- src/api/api.js
+++ src/api/api.js
@@ -1,14 +1,15 @@
-// import axios from "axios";
 const axios = require("axios");
-
-const BASE_URL = "https://pixabay.com/api/";
+axios.defaults.baseURL = "https://pixabay.com/api/";
 const API_KEY = "23855124-9744a76c55c8d410f772c2e55";
-const PER_PAGE = 12;
 
-async function fetchImages(searchQuery, page) {
+async function fetchImages({
+  searchQuery = "",
+  currentPage = 1,
+  pageQuantity = 12,
+}) {
   try {
     const response = await axios.get(
-      `${BASE_URL}?q=${searchQuery}&page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=${PER_PAGE}`
+      `/?q=${searchQuery}&page=${currentPage}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=${pageQuantity}`
     );
     return response.data;
   } catch (error) {
